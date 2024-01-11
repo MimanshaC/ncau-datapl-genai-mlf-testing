706835f7be079c71ee58a1beb87bc7f2fff58d4f
Mimansha-news <151734989+MimanshaC@users.noreply.github.com>
2024-01-12 05:19:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 # Load environment variables into constants - key error raised if not set
-#ENV = "nprod"
-ENV = os.environ["ENVIRONMENT"]
-MODEL_NAME_PREFIX = os.environ["MODEL_NAME_PREFIX"]
-#MODEL_NAME_PREFIX = "xgb_churn_prediction"
+ENV = "nprod"
+#ENV = os.environ["ENVIRONMENT"]
+#MODEL_NAME_PREFIX = os.environ["MODEL_NAME_PREFIX"]
+MODEL_NAME_PREFIX = "xgb_churn_prediction"
 MODEL_NAME_CUSTOM = f"{MODEL_NAME_PREFIX}_custom"
 
-#PIPELINE_BUCKET = "gs://mlops-ncau-data-nprod-aitrain"
-PIPELINE_BUCKET = os.environ["PIPELINE_BUCKET"]
+PIPELINE_BUCKET = "gs://mlops-ncau-data-nprod-aitrain"
+#PIPELINE_BUCKET = os.environ["PIPELINE_BUCKET"]
 PIPELINE_ROOT = f"{PIPELINE_BUCKET}/{MODEL_NAME_PREFIX}"
 
 # Variables to change per project
