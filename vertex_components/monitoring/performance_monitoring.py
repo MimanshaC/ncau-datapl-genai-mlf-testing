0ea5952c09f23d929af78a3605300efb939c583a
Mimansha-news <151734989+MimanshaC@users.noreply.github.com>
2024-02-16 12:48:10 +1100

    Update performance_monitoring.py

--- vertex_components/monitoring/performance_monitoring.py
+++ vertex_components/monitoring/performance_monitoring.py
@@ -76,7 +76,7 @@ def evaluate_performance_monitoring(
         FROM `{project_id}.mlf_testing.customer_churn_data`
     """
     dtypes = {
-         'user_id': str,
+         'user_id': int,
          'month': int,
          'package_duration': int,
          'segment': int,
@@ -113,7 +113,7 @@ def evaluate_performance_monitoring(
          'frequency30_change_LT_Neg_60': int,
          'price_up': float,
          'section_sops_30': int,
-         'target_binary': str,
+         'target_binary': int,
          'subscription_id': int,
          'pcsid': int,
          'tenure_grouping': int,
